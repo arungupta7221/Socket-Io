3e1156c9f7f81258afd605a453a701db5375cac4
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2023-02-18 22:16:22 +0530

    added code with some functions

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const server = http.createServer(app)
 const io = socketio(server)
 app.use('/', express.static(__dirname + '/public'))
 io.on('connection', (socket) => {
-  console.log('a user connected', socket.id)
+  socket.on('msg_send', (data) => {
+    console.log(data)
+    socket.broadcast.emit('msg_rcvd', data)
+  })
+  // setInterval(() => {
+  //   socket.emit('from_server')
+  // }, 2000)
 })
+
 server.listen(3000, () => {
   console.log('server started')
 })
